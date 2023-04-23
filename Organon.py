35650d6142706a6c61c8d646c8f2db51b0f450c2
Defalt Astra <39121090+defaltastra@users.noreply.github.com>
2023-04-23 15:55:25 +0000

    Update Organon.py

--- Organon.py
+++ Organon.py
@@ -4,9 +4,6 @@ import shutil
 import time
 import getpass
 
-# mapping each extension with its corresponding folder
-# For example, 'jpg', 'png', 'ico', 'gif', 'svg' files will be moved to 'images' folder
-# feel free to change based on your needs
 extensions = {
     ".jpg": "images",
     ".jpeg": "images",
@@ -51,38 +48,38 @@ def organize_downloads_folder():
     print("Organizing Downloads Folder...")
     time.sleep(1)
     
-    name = getpass.getuser()
-    path = os.path.expanduser(f"~\\Downloads")
+    username = getpass.getuser()
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
     verbose = 1
     
-    for extension, folder_name in extensions.items():
-        # get all the files matching the extension
-        files = glob.glob(os.path.join(path, f"*{extension}"))
-        if verbose == 1:
-            print(f"[*] Found {len(files)} files with {extension} extension")
-        if files:
-            # create the folder if it does not exist before
-            folder_path = os.path.join(path, folder_name)
-            os.makedirs(folder_path, exist_ok=True)
-            if verbose == 1:
-                print(f"[+] Created {folder_path}")
-        for file in files:
-            # for each file in that extension, move it to the corresponding folder
-            basename = os.path.basename(file)
-            dst = os.path.join(path, folder_name, basename)
-            if os.path.abspath(file) == os.path.abspath(dst):
-                continue  # Skip if source and destination are the same file
-            if verbose == 1:
-                print(f"[*] Moving {file} to {dst}")
-            shutil.move(file, dst)
+    files = [f for ext in extensions for f in glob.glob(os.path.join(downloads_path, f"*{ext}"))]
+    if verbose:
+        print(f"[*] Found {len(files)} files to organize")
+        
+    for file in files:
+        ext = os.path.splitext(file)[1].lower()
+        folder_name = extensions.get(ext, "others")
+        folder_path = os.path.join(downloads_path, folder_name)
+        os.makedirs(folder_path, exist_ok=True)
+        dst = os.path.join(folder_path, os.path.basename(file))
+        if os.path.abspath(file) == os.path.abspath(dst):
+            continue
+        if verbose:
+            print(f"[*] Moving {file} to {dst}")
+        shutil.move(file, dst)
 
 def add_to_startup():
-    file_path = os.path.abspath(__file__)
-    bat_path = os.path.join(os.path.expanduser("~"), "AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
-    with open(os.path.join(bat_path, "open.bat"), "w+") as bat_file:
-        bat_file.write(f'''@echo off
+    ans = input("Do you want to add the script to the system startup? (y/n): ").lower()
+    if ans == "y":
+        file_path = os.path.abspath(__file__)
+        bat_path = os.path.join(os.getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+        with open(os.path.join(bat_path, "open.bat"), "w+") as bat_file:
+            bat_file.write(f'''@echo off
 python "{file_path}"
 exit ''')
+        print("Script added to system startup.")
+    else:
+        print("Script not added to system startup.")
 
 if __name__ == "__main__":
     organize_downloads_folder()
